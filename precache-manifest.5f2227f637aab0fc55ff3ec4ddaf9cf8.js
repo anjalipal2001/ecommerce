7eec7ad4315011821baa6c4642d95c20c09d77c4
anjalipal2001 <ap7197339@gmail.com>
2021-04-10 21:27:46 +0530

    Updates

---
+++ precache-manifest.5f2227f637aab0fc55ff3ec4ddaf9cf8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c7aa01fe46557bcc69be3670e25e38",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "fdab36563037d595e423",
+    "url": "/ecommerce/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "db3e74c1141e8d2b8a5e",
+    "url": "/ecommerce/static/css/main.9d988fb0.chunk.css"
+  },
+  {
+    "revision": "fdab36563037d595e423",
+    "url": "/ecommerce/static/js/2.297d7167.chunk.js"
+  },
+  {
+    "revision": "20377f3c2f2703e596dd85276960f64e",
+    "url": "/ecommerce/static/js/2.297d7167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3e74c1141e8d2b8a5e",
+    "url": "/ecommerce/static/js/main.f0f16f5e.chunk.js"
+  },
+  {
+    "revision": "480b7ec90b6c3279eba2",
+    "url": "/ecommerce/static/js/runtime-main.ec4e4ff1.js"
+  },
+  {
+    "revision": "a6827cf5a483c13359d03d74eeb3951e",
+    "url": "/ecommerce/static/media/1.a6827cf5.jpg"
+  },
+  {
+    "revision": "b35d99509a21157f84888a170ddfbfed",
+    "url": "/ecommerce/static/media/2.b35d9950.jpg"
+  },
+  {
+    "revision": "5989ebbfe5acdbbce7d80b77164a9b0c",
+    "url": "/ecommerce/static/media/3.5989ebbf.jpg"
+  },
+  {
+    "revision": "5abb223434e6ae5ebfb6091d2ffe2db6",
+    "url": "/ecommerce/static/media/4.5abb2234.jpg"
+  },
+  {
+    "revision": "414026f730ac98c9ae5761c0efe6efa1",
+    "url": "/ecommerce/static/media/5.414026f7.jpg"
+  },
+  {
+    "revision": "54f2288fa1db1b0569dd4d74aa200172",
+    "url": "/ecommerce/static/media/card.54f2288f.jpg"
+  },
+  {
+    "revision": "84799a34e5bbc050be6e0f284ad7b26f",
+    "url": "/ecommerce/static/media/emptyBookmarks.84799a34.svg"
+  },
+  {
+    "revision": "93ba78358a09a1026c6cd1322a084502",
+    "url": "/ecommerce/static/media/emptyCart.93ba7835.svg"
+  },
+  {
+    "revision": "b9dcccae818c5cbea8e792529ed61557",
+    "url": "/ecommerce/static/media/success.b9dcccae.svg"
+  }
+]);
