0425c2c6d9bf41dbafbad930b572327835d0e20f
Anjali Pal <50593256+anjalipal2001@users.noreply.github.com>
2021-04-11 16:54:27 +0530

    Add files via upload

---
+++ build/precache-manifest.feeed548d569fe9f0e8dcc701fd5da38.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e94adc341a6e2674fb65fbe60d20847",
+    "url": "/reactjsecommerce/index.html"
+  },
+  {
+    "revision": "202597bf2373f5bda111",
+    "url": "/reactjsecommerce/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "18c95717eea3116c7a3a",
+    "url": "/reactjsecommerce/static/css/main.ae8c0145.chunk.css"
+  },
+  {
+    "revision": "202597bf2373f5bda111",
+    "url": "/reactjsecommerce/static/js/2.704f30df.chunk.js"
+  },
+  {
+    "revision": "2b9104cc996cacb095306a0d81fa5a1c",
+    "url": "/reactjsecommerce/static/js/2.704f30df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c95717eea3116c7a3a",
+    "url": "/reactjsecommerce/static/js/main.df3aab03.chunk.js"
+  },
+  {
+    "revision": "0358119d7621fba597e2",
+    "url": "/reactjsecommerce/static/js/runtime-main.4532451c.js"
+  },
+  {
+    "revision": "a6827cf5a483c13359d03d74eeb3951e",
+    "url": "/reactjsecommerce/static/media/1.a6827cf5.jpg"
+  },
+  {
+    "revision": "b35d99509a21157f84888a170ddfbfed",
+    "url": "/reactjsecommerce/static/media/2.b35d9950.jpg"
+  },
+  {
+    "revision": "5989ebbfe5acdbbce7d80b77164a9b0c",
+    "url": "/reactjsecommerce/static/media/3.5989ebbf.jpg"
+  },
+  {
+    "revision": "5abb223434e6ae5ebfb6091d2ffe2db6",
+    "url": "/reactjsecommerce/static/media/4.5abb2234.jpg"
+  },
+  {
+    "revision": "414026f730ac98c9ae5761c0efe6efa1",
+    "url": "/reactjsecommerce/static/media/5.414026f7.jpg"
+  },
+  {
+    "revision": "54f2288fa1db1b0569dd4d74aa200172",
+    "url": "/reactjsecommerce/static/media/card.54f2288f.jpg"
+  },
+  {
+    "revision": "84799a34e5bbc050be6e0f284ad7b26f",
+    "url": "/reactjsecommerce/static/media/emptyBookmarks.84799a34.svg"
+  },
+  {
+    "revision": "93ba78358a09a1026c6cd1322a084502",
+    "url": "/reactjsecommerce/static/media/emptyCart.93ba7835.svg"
+  },
+  {
+    "revision": "cd302fef4b563bd6e8951bef5effa8ee",
+    "url": "/reactjsecommerce/static/media/icon.cd302fef.svg"
+  },
+  {
+    "revision": "159260d8a74b0cbad3439696b1da28f7",
+    "url": "/reactjsecommerce/static/media/logo.159260d8.svg"
+  },
+  {
+    "revision": "b9dcccae818c5cbea8e792529ed61557",
+    "url": "/reactjsecommerce/static/media/success.b9dcccae.svg"
+  }
+]);
